df5047fd56f2eeba87d7a9163b540c07b4b70a72
Gino Zaccardelli <ginozacc@gmail.com>
2020-09-10 20:16:15 -0400

    After many tribulations...

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Wendy's Burger");
+INSERT INTO burgers (burger_name) VALUES ("McDonalds' Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Burger King Burger", true);
